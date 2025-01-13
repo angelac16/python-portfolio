dbb9349540c0632fb3a4f7093480359b95a5c764
angelac16 <acai2@cps.edu>
2025-01-13 11:42:39 -0600

    Add files via upload

---
+++ madlib (1).py
@@ -0,0 +1,38 @@
+#Angela Cai
+#12/9/2024
+#MadLibs
+
+#Function
+def gameofTrickTreating():
+    print ("Let's go play a fun game of Trick-o-Treating!")
+    print ("To get started, please enter interesting words, thank you!")
+    adjOne = input("Enter an adjective: ")
+    adjTwo = input("Enter a mysterious adjective: ")
+    adjThree = input("Enter a silly adjective: ")
+    adjFour = input("Enter another silly adjective: ")
+    nounOne = input("Enter a noun: ")
+    nounTwo = input("Enter your most expensive costume: ")
+    animalTwo = input("Enter a sea animal: ")
+    animalThree = input("Enter a plural energetic animal: ")
+    chocolate = input("Enter a type of chocolate that begins with either S or T: ")
+    nonchocolate = input("Enter a type of candy that is not chocolate: ")
+    candy = input("Enter any type of candy: ")
+    color = input("Enter a color")
+    numberOne = input("Enter a number greater than 10, but less than 15")
+    numberTwo = input("Enter a three digit number")
+    verbOne = input("Enter a verb: ")
+    verbTwo = input("Enter a verb that is one of the following: ate, swallowed, or devoured: ")
+    print("One day, as I was walking in a sketchy neighborhood and going from house to house at 2AM, we saw a " + adjOne + " " + nounOne + " " + "driving a car with a sign saying, 'ON THE WAY TO GO TRICK-TREATING'" + ".")
+    print("We hurried out of the " + adjOne + nounOne + "'s way quickly.")
+    print("As we were running to get out of " + nounOne + "'s" + " way, we saw another " + adjTwo + DOG" " + "and " + adjThree + " " + animalTwo + " crawling our way" + ".")
+    print("We quickly hid away from the " + adjTwo + " and " + adjThree + " " + animalTwo + "'s way. In our heads, we only wanted candy, but there were few " + adjFour + " coming our way left and right" + ".")
+    print("As the " + color + " " + animalTwo + " " + verbOne + " up to me, it reached out " + numberOne + " pieces of " + candy + " to me" + ".")
+    print("I thanked the " + color + animalTwo + " , afterwards I kept on walking to houses to get candy and when I knocked on someone's door, they wore a " + nounTwo + " costume and their son behind them wore a pizza costume" + ".")
+    print("The woman's son generously gave me " + numberTwo + " pieces of " + nonchocolate + " " + "and" + " " + chocolate + " candy" + ".")
+    print("I left their house with a huge smile on my face, but as I turned back to leave, " + numberOne + " " + animalThree + " " + verbTwo + " me, and I died on October 31st, 2020" + ".")
+
+#Main
+gameofTrickTreating()
+
+
+
