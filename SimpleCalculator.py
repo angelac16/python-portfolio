7aff6cd402eb462602c351497d5c90063c6a5d55
angelac16 <acai2@cps.edu>
2025-01-14 10:42:48 -0600

    Update SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -6,22 +6,22 @@
 #Functions
 #Adds two numbers together and prints the result
 def add(num1, num2):
-    result = num1 + num2
-    print(result)
+    result = num1 + num2 #This function is to add two numbers together
+    print(result)#This is what causes the dividing to be seen for the player. 
 def subtract(num1, num2):
-    result = num1 - num2
-    print(result)
+    result = num1 - num2 #This function is to subtract two numbers from each other.
+    print(result)#This is what causes the dividing to be seen for the player.
 def multiply(num1, num2):
-    result = num1*num2
-    print(result)
+    result = num1*num2 #This function is to multiply two numbers together
+    print(result)#This is what causes the dividing to be seen for the player.
 def divide(num1, num2):
-    result = num1/num2
-    print(result)
+    result = num1/num2 #This function is to divide two numbers
+    print(result) #This is what causes the dividing to be seen for the player.
 
 print("Welcome to simple calculator")
 def simpleCalculator():
     while True:
-        print("Please select an operation: ")
+        print("Please select an operation: ") #This is for the player to pick which operation they want to use on the calculator
         print("""1. Add
 2. Subtract
 3. Multiply
@@ -29,24 +29,24 @@ def simpleCalculator():
 5. Quit""")
         operation = int(input("(1-5) Option: "))
         if operation == 1:
-            int1 = int(input("Enter the first number: "))
-            int2 = int(input("Enter the second number: "))
-            add(int1, int2)
-        elif operation == 2:
+            int1 = int(input("Enter the first number: "))#The player enters their first number
+            int2 = int(input("Enter the second number: "))#The player enters their second number
+            add(int1, int2) #This function is to add two numbers together
+        elif operation == 2: #If they typed in "2", they picked the subtraction function on the calculator
             int1 = int(input("Enter the first number: "))
             int2 = int(input("Enter the second number: "))
             subtract(int1, int2)
-        elif operation == 3:
+        elif operation == 3: #If they typed in "3", they picked the multiplication function on the calculator
             int1 = int(input("Enter the first number: "))
             int2 = int(input("Enter the second number: "))
             multiply(int1, int2)
-        elif operation == 4:
+        elif operation == 4: #If they typed in "2", they picked the division function on the calculator
             int1 = int(input("Enter the first number: "))
             int2 = int(input("Enter the second number: "))
             divide(int1, int2)
-        elif operation == 5:
+        elif operation == 5: #If they typed in "2", they picked the quit button on the calculator
             print("Goodbye.")
-            break
+            break #This ends the continuous loop of using the calculator.
 
 #Main
 simpleCalculator()
