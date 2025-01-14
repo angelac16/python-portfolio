2819d641e6439150fbaa2cdcf4213f705595a1f3
angelac16 <acai2@cps.edu>
2025-01-14 10:44:39 -0600

    Update MultiplicationQuiz.py

--- MultiplicationQuiz.py
+++ MultiplicationQuiz.py
@@ -9,21 +9,22 @@ score = 0
 #Functions
 def multiplication_quiz():
     print("Welcome to the Multiplication Quiz!") #Welcoming the player
-    amountOfQuestions = int(input("How many multiplication questions would you like?: "))
+    amountOfQuestions = int(input("How many multiplication questions would you like?: "))#The player decides on how many questions they want to be asked.
     for i in range(amountOfQuestions):
         num1 = random.randint(1,20) #Integer
         num2 = random.randint(1,20) #Integer
         print("What is " + str(num1) + " x " + str(num2)) #Converting the number to a string to add the strings with the numbers together
         playersAnswer = int(input("Please enter your answer: "))
-        solution = num1*num2
+        solution = num1*num2#This multiplies two numbers together, but does not show the audience what the solution is.
         if playersAnswer == solution:
-            global score
-            print("Yay, you are correct!")
-            score = score + 1
+            global score#Global allows to change a variable value wherever it is and needed.
+            print("Yay, you are correct!")#The player got the multiplication question right.
+            score = score + 1#This is a score baord to keep track of the score when the player gets a question right each time.
         else:
-            print("Oh no, you got it incorrect.")
-        print("Your score is " + str(score) + "out of " + str(amountOfQuestions))
+            print("Oh no, you got it incorrect.")#The player got the answer wrong for the question.
+        print("Your score is " + str(score) + " out of " + str(amountOfQuestions))#This is the player's total score at the end of the game
 
 #Main
 multiplication_quiz()
+
 
